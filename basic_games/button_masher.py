f7b5571118f347d2f3a1cb184ec4e7e02b50a23c
contactandy <contactandy@duck.com>
2023-12-03 15:08:59 -0500

    Convert button masher to app style

--- basic_games/button_masher.py
+++ basic_games/button_masher.py
@@ -1,37 +1,87 @@
+"""A button mashing game."""
 import pygame
 
-pygame.init()
-pygame.display.set_caption("Mash buttons!")
-window = pygame.display.set_mode((200, 200))
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-clock = pygame.time.Clock()
-countdown = 5
-timer_event = pygame.USEREVENT+1
-pygame.time.set_timer(timer_event, 1000)
+class Counter:
+    """Counter with a draw method"""
+    def __init__(self, position):
+        self.counter = 0
+        self.pos = position
+        self.font = pygame.font.SysFont(None, 100)
 
-font = pygame.font.SysFont(None, 100)
-text = font.render(str(countdown), True, (0, 128, 0))
+    def inc(self):
+        self.counter += 1
+
+    def draw(self, surf):
+        display = f"Score: {self.counter}"
+        text = self.font.render(display, True, BLACK)
+        rect = text.get_rect(topleft = self.pos)
+        surf.blit(text, rect)
 
 
-keycounter = 0
+class Timer:
+    """Timer for seconds with a draw method"""
+    def __init__(self, duration, position):
+        self.clock = pygame.time.Clock()
+        self.event = pygame.USEREVENT
+        self.remaining = duration
+        pygame.time.set_timer(self.event, 1000)
+        self.done = False
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        elif event.type == timer_event:
-            countdown -= 1
-            text = font.render(str(countdown), True, (0, 128, 0))
-            if countdown == 0:
-                pygame.time.set_timer(timer_event, 0)                
-                text = font.render(str(keycounter), True, (0, 128, 0))
-        elif event.type == pygame.KEYDOWN:
-            if countdown > 0:
-                keycounter += 1
+        self.pos = position
 
-    window.fill((255, 255, 255))
-    text_rect = text.get_rect(center = window.get_rect().center)
-    window.blit(text, text_rect)
-    pygame.display.flip()
+        self.font = pygame.font.SysFont(None, 100)
+        self.max_digits = len(str(duration))
 
+    def tick(self):
+        self.remaining -= 1
+        if self.remaining == 0:
+            pygame.time.set_timer(self.event, 0)
+            self.done = True
+
+    def draw(self, surf):
+        display = f"Time: {self.remaining:0{self.max_digits}}"
+        text = self.font.render(display, True, BLACK)
+        rect = text.get_rect(topleft=self.pos)
+        surf.blit(text, rect)
+
+class App:
+    """Play button masher"""
+
+    def __init__(self):
+        """Initialize pygame and the application"""
+        pygame.init()
+        pygame.display.set_caption("Mash buttons!")
+        self.screen = pygame.display.set_mode((500, 500))
+
+        self.timer = Timer(10, (0,0))
+        self.counter = Counter((0, 60))
+
+        self.running = True
+
+    def run(self):
+        """Run the main event loop."""
+        font = pygame.font.SysFont(None, 100)
+        text = font.render(str(self.timer.remaining), True, (0, 128, 0))
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                elif event.type == self.timer.event:
+                    self.timer.tick()
+                elif event.type == pygame.KEYDOWN:
+                    if not self.timer.done:
+                        self.counter.inc()
+            self.draw()
+
+    def draw(self):
+        self.screen.fill(WHITE)
+        self.timer.draw(self.screen)
+        self.counter.draw(self.screen)
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    App().run()
