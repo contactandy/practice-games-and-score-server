2cfbb4b8b2643699cd204743bbabdc53093c897a
contactandy <contactandy@duck.com>
2023-12-27 13:00:37 -0500

    Unify games under entrypoint
    Set __init__.py to do CLI game selection and initialization.

---
+++ basic_games/__init__.py
@@ -0,0 +1,26 @@
+import argparse
+import button_masher
+import good_timing
+
+GAME_CATALOGUE = {
+    "masher": {"module": button_masher, "server_name": "BUTTON"},
+    "clicker": {"module": good_timing, "server_name": "TIMING"},
+}
+
+
+PARSER = argparse.ArgumentParser(description="Play a game to get a high score!")
+PARSER.add_argument(
+    "--game", choices=GAME_CATALOGUE.keys(), required=True, help="select a game"
+)
+PARSER.add_argument("--username", required=True, help="enter your username")
+
+
+def main():
+    args = PARSER.parse_args()
+
+    score = GAME_CATALOGUE[args.game]["module"].App().run()
+    print(f"Congrats, {args.username}, you got a score of {score}!")
+
+
+if __name__ == "__main__":
+    main()
